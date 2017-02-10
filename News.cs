571230e74a8678652a934b47e9a8946e906c62b9
Savelid <savelid@gmail.com>
2017-02-10 15:45:53 +0100

    Class for news, picture and category

---
+++ News.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libraryNaturguiden
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public System.DateTime Date { get; set; }
+        public string Creator { get; set; }
+        public string Heading { get; set; }
+        public string Text { get; set; }
+        public int PictureId { get; set; }
+        public Picture Picture { get; set; }
+        public string LinkUrl { get; set; }
+        public string LinkText { get; set; }
+        public int Position { get; set; }
+    }
+}
---
+++ Picture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libraryNaturguiden
+{
+    public class Picture
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Url { get; set; }
+        [MaxLength(99)]
+        public string Alt { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        [MaxLength(99)]
+        public string Owner { get; set; }
+        [MaxLength(49)]
+        public string Format { get; set; }
+        public List<int> Categories { get; set; }
+    }
+}
