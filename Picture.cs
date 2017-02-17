5ec1bc9951b6570302ed268732ebb9508e1c1ff5
Savelid <savelid@gmail.com>
2017-02-17 13:44:27 +0100

    New props for resized image urls

--- Picture.cs
+++ Picture.cs
@@ -10,8 +10,11 @@ namespace libraryNaturguiden
     public class Picture
     {
         public int Id { get; set; }
+        public string FileName { get; set; }
         [Required]
         public string Url { get; set; }
+        public string FormatedUrl { get; set; }
+        public string ThumbUrl { get; set; }
         [MaxLength(99)]
         public string Alt { get; set; }
         [DataType(DataType.Date)]
