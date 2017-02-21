6e1b2282597e90a9092ca589cf678592049f2973
Savelid <savelid@gmail.com>
2017-02-21 09:56:13 +0100

    Added a pucture container to hold pictures and baseUrl

--- Picture.cs
+++ Picture.cs
@@ -10,12 +10,16 @@ namespace libraryNaturguiden
     public class Picture
     {
         public int Id { get; set; }
-        public string FileName { get; set; }
         [Required]
+        [Display(Name="File Name")]
+        public string FileName { get; set; }
         public string Url { get; set; }
+        [Display(Name = "Formated Url")]
         public string FormatedUrl { get; set; }
+        [Display(Name = "Thumbnail Url")]
         public string ThumbUrl { get; set; }
         [MaxLength(99)]
+        [Display(Name = "Alt. text")]
         public string Alt { get; set; }
         [DataType(DataType.Date)]
         public DateTime Date { get; set; }
---
+++ PictureContainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libraryNaturguiden
+{
+    public class PictureContainer
+    {
+        public string UrlBase = "http://admin.naturguiden.com";
+        public Picture[] Pictures { get; set; }
+        public PictureContainer(Picture[] pictures)
+        {
+            Pictures = pictures;
+        }
+        public PictureContainer(Picture picture)
+        {
+            Pictures = new Picture[] { picture };
+        }
+    }
+}
